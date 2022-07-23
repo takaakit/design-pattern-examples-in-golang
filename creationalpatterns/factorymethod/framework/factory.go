33388070f6ffb8572c131340e5b5a1e6a56e014b
takaakit <takaaki.teshima.dev@gmail.com>
2022-07-23 18:09:12 +0900

    Edit comments

--- creationalpatterns/factorymethod/framework/factory.go
+++ creationalpatterns/factorymethod/framework/factory.go
@@ -26,11 +26,12 @@ func NewFactory() *Factory {
 // (Evaluating the GO Programming Language with Design Patterns)
 func (f *Factory) Create(ifactory IFactory, owner string) Product {
 	// ˅
-	// Write pre-creation code here, if any.
+	// Write pre-creation code here.
 
+	// Encapsulate the knowledge of which Product subclass to create and move this knowledge out of the framework.
 	product := ifactory.CreateProduct(owner)
 
-	// Write post-creation code here, if any.
+	// Write post-creation code here.
 
 	return product
 	// ˄
--- creationalpatterns/prototype/framework/manager.go
+++ creationalpatterns/prototype/framework/manager.go
@@ -30,7 +30,7 @@ func (m *Manager) RegisterDisplay(displayName string, display Display) {
 func (m *Manager) GetDisplay(displayName string) Display {
 	// ˅
 	display := m.displays[displayName]
-	return display.Clone()
+	return display.Clone() // Create a new object by asking a concrete class to clone itself. Therefore, do not need to know the concrete Display class name.
 	// ˄
 }
 
